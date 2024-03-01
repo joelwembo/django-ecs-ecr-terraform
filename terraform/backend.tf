accd6c4c258269061cd724ec63782479378a5245
Joel Wembo <joelwembo@outlook.com>
2024-03-02 02:47:50 +0800

    updated terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {  
     hostname="app.terraform.io"  
-    token = "kbrAiDdnwCKEYg.atlasv1.9COhLimGMbbyuQVLFk2ZT7ezuIZ63UfaXl6DsUCTtD9ISmWLHnDk02Vt68DcACPY7W4"
+    token = ""
     organization = "prodxcloud" 
     workspaces {
       prefix = "prodxcloud-" 
