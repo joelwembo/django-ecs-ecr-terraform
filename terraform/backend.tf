43030fef197723c0506ab6e1fb867ce340e8ad9d
Joel Wembo <joelwembo@outlook.com>
2024-03-02 23:28:41 +0800

    added st

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {  
     hostname="app.terraform.io"  
-    token = "yyRRkpzskzjzuQ.atlasv1.f9EaijAOOibTbMH32jRrR4O3X2Y9f4OA8jFj8dizVAHymPoh1doMNWyPurC1tqlxamQ"
+    token = ""
     organization = "prodxcloud" 
     workspaces {
       prefix = "prodxcloud-" 
