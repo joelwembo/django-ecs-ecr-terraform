a44a3520c7f3b878f129e2f52c33e692a9ba746e
Joel Wembo <joelwembo@outlook.com>
2024-03-02 23:23:42 +0800

    added security components

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {  
     hostname="app.terraform.io"  
-    token = ""
+    token = "yyRRkpzskzjzuQ.atlasv1.f9EaijAOOibTbMH32jRrR4O3X2Y9f4OA8jFj8dizVAHymPoh1doMNWyPurC1tqlxamQ"
     organization = "prodxcloud" 
     workspaces {
       prefix = "prodxcloud-" 
